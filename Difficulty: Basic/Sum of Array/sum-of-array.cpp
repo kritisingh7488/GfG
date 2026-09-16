bd6fcd98db9889c7b393812b772177ebc16d61f5
Kumari Kriti Singh <149036472+kritisingh7488@users.noreply.github.com>
2026-09-16 22:48:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int arraySum(vector<int>& arr) {
+        int n = arr.size();
+        int sum =0;
+        for(int i =0; i<n;i++){
+            sum += arr[i];
+        }
+        return sum;
+        
+    }
+};
