5373eab316ce13bbfa5ecca401ea85171a74efa3
Kumari Kriti Singh <149036472+kritisingh7488@users.noreply.github.com>
2026-09-21 22:07:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Linked List/print-linked-list.cpp
@@ -0,0 +1,32 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> printList(Node *head) {
+        vector<int> arr;
+        while(head!=NULL){
+            arr.push_back(head->data);
+            head=head->next;
+        }
+        return arr;
+        
+        
+    }
+};
